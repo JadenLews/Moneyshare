5e9914aeaeffda8dd2c8139a44926085c30eb5c0
JadenLews <119870564+JadenLews@users.noreply.github.com>
2024-03-17 19:04:01 -0400

    Add files via upload

--- DirectedStudy/main_file.py
+++ DirectedStudy/main_file.py
@@ -1,4 +1,5 @@
 from user import User
+import time
 
 def valid_account(id):
     #WIP
@@ -16,7 +17,7 @@ def choices(user):
     inp = input('What would you like to do?\nSEND, RECEIVE, CHECK BALANCE\n')
     if inp == 'check balance':
         check_balance(user)
-    elif inp == 'reveive':
+    elif inp == 'receive':
         receive(user)
     elif inp == 'send':
         send(user)
@@ -28,29 +29,42 @@ def check_balance(user):
 
 def send(user):
     #send to which user
-    other_user = 1
-    amount = input('How much money are you sending?\n')
-    amount = int(amount)
-    if not user.balance_check(amount):
-        print('Insufficient Balance\n')
+    amount = int(input('How much money are you sending?\n'))
+    p_number = int(input('What is the phone number of who you are sending money to\n'))
+    code_1 = int(input('Enter Code 1\n'))
+    code_1_check = user.validate_code_1(p_number, code_1)
+    #Check
+    if code_1_check:
+        #True
+        #generate code 2
+        user.make_transaction(amount, 'subtract')
+        code_two = user.generate_code_2(amount, p_number)
+        print(f"Code 2 is {code_two}\n")
+        print(f"Balance is now {user.account_balance}\n")
+        time.sleep(3)
         choices(user)
+
     else:
-        #input code 1
-        input_code_1 = int(input('Input code 1\n'))
-        if not user.validate(other_user, input_code_1):
-            print('There has been an error, try again')
-        else:
-            #generate code 2
-            code_two = user.generate_code
-            print(f"Code 2 is {code_two}\n")
-            user.make_transaction(amount, 'subtract')
-            choices(user)
+        #error False
+        print('There has been an error, try again')
 
 
 def receive(user):
     amount = int(input('How much money are you receiving?\n'))
-    code_one = user.generate_code
+    p_number = int(input('What is the phone number of who is sending money?\n'))
+    code_one = user.generate_code_1(amount, p_number)
     print(f"Code 1 is {code_one}\n")
+    time.sleep(3)
+    code_2 = int(input('Enter Code 2\n'))
+    code_2_check = user.validate_code_2(p_number, code_2)
+    #Check
+    if code_2_check:
+        #True so add funds
+        user.make_transaction(amount, 'add')
+        print(f"{amount} has been added to your wallet\nYour balance is now {user.account_balance}\n")
+        time.sleep(3)
+        choices(user)
+
 
 def valid_code(user, code):
     #use code and user to ensure correctly generated code
--- DirectedStudy/user.py
+++ DirectedStudy/user.py
@@ -1,10 +1,11 @@
 class User:
-    def __init__(self, p_name, p_unique, p_input_balance, currency):
+    def __init__(self, p_name, p_number, p_input_balance, currency):
         self.name = p_name
         #scramble the unique indentifier to not be tracable
-        self.unique_identifier = p_unique
+        self.unique_identifier = p_number
         self.account_balance = p_input_balance
         self.currency = currency
+        self.transaction_log = {}
         #user identifier has to be unique
     def balance_check(self, amount):
         if self.account_balance < amount:
@@ -17,9 +18,20 @@ class User:
             self.account_balance = self.account_balance + amount
         elif action == 'subtract':
             self.account_balance = self.account_balance - amount
-
-    def generate_code(self):
+    
+    def generate_code_1(self, amount, phone_number):
         return 123456789
 
-    def validate(self, other, code):
+    def validate_code_1(self, phone_number, code):
         return True
+    
+    def generate_code_2(self, amount, phone_number):
+        return 123456789
+
+    def validate_code_2(self, phone_number, code):
+        return True
+    
+    def log_transaction(self, p_number, amount):
+        #if user does not exist in current dictionary log then make new dictionary item
+        #if user does exist, add log to the list of transactions with user
+        print()
